67e7e58e5db145a5fd63319fabad03e8952d4f20
shodiior <shoazizovshodiyor6@gmail.com>
2026-06-23 05:28:27 +0500

    Update proktal.py

--- proktal.py
+++ proktal.py
@@ -3,7 +3,7 @@ import threading
 import telebot
 from flask import Flask
 
-TOKEN = "8237088507:AAFmbkYCiX4bOadOKhqDDMu8wV76TuE907I"
+TOKEN = "8237088507:AAFmbkYCiX4b0ad0KhqDDMu8wV76TuE907I"
 bot = telebot.TeleBot(TOKEN)
 
 def get_back_keyboard():
